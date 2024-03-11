e9dbc7e973db355b7d2cbb31299f9418748de3ec
Shahad Hassan <shahadg1983@gmail.com>
2024-03-11 14:08:11 +0530

    Update url

--- frontend/src/Charts/SectorData.jsx
+++ frontend/src/Charts/SectorData.jsx
@@ -9,7 +9,7 @@ const SectorData = () => {
 
     const fetchData = async () => {
         try {
-            const response = await axios.get("http://localhost:4000/api/users/sector");
+            const response = await axios.get("https://insight-lens-beta.vercel.app/api/users/sector");
             const data = response.data;
             setUpChart(data)
 

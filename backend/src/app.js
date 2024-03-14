085c4b78548f90f3ca112783c489224456a5499f
Shahad Hassan <shahadg1983@gmail.com>
2024-03-14 12:23:22 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ["https://insight-lens.vercel.app/"],
     credentials: true
 }))
 
